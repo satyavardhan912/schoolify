5750c6866a865f8f9d853a90b09c243128ddf881
satyavardhan912 <satyavardhan811@gmail.com>
2025-11-27 22:09:05 +0000

    Updated the main.py to have the users and students router

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from app.routes import auth
+from app.routes import auth, users,students
 
 load_dotenv()
 
 app = FastAPI(title="Schoolify")
 
-app.include_router(auth.router) 
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(students.router)
 
 @app.get("/healthz")
 def health():
