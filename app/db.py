01c2278432a2f0a7ce7e978ad51f9b2d9dadbbf8
satyavardhan912 <satyavardhan811@gmail.com>
2025-11-30 20:07:34 +0000

    Make changes to mongo url, include localhost and ping -s

--- app/db.py
+++ app/db.py
@@ -4,11 +4,20 @@ from pymongo import MongoClient
 
 load_dotenv()
 
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://mongo:27017/schoolify")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/schoolify")
 
 client = MongoClient(MONGODB_URI)
 
-db = client.get_default_database() if client.get_default_database() else client["schoolify"]
+_default_db = client.get_default_database()
+db = _default_db if _default_db is not None else client["schoolify"]
 
 def get_users_collection():
     return db["users"]
+
+def ping():
+    """Return True if Mongo responds to ping."""
+    try:
+        client.admin.command("ping")
+        return True
+    except Exception:
+        return False
