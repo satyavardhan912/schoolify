58d7fb9fc0ff79d2c2a4055b22bdd0017023d88b
satyavardhan912 <satyavardhan811@gmail.com>
2025-12-09 17:41:41 +0000

    updated CORS and bcrypt version

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes import auth, users, students, exams
 
 load_dotenv()
 
 app = FastAPI(title="Schoolify")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
